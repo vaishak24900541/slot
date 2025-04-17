cafcded6550a4a457e3f07b6b712ec59d7c62f30
mvaishak <mvaishak564@gmail.com>
2025-04-17 21:29:27 +0530

    Success

--- vijay/vijay/wsgi.py
+++ vijay/vijay/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for JD project.
+WSGI config for vijay project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vijay.settings')
 
 application = get_wsgi_application()
